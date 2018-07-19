f027875a16860af8feface0c6f65d772a3946d76
DESKTOP-DQ3ODV6\MARIEYFRAN <falvarino@ximiodev.com>
2018-07-19 10:52:48 -0300

    reset

--- src/android/OnNotificationOpenReceiver.java
+++ src/android/OnNotificationOpenReceiver.java
@@ -19,7 +19,7 @@ public class OnNotificationOpenReceiver extends BroadcastReceiver {
         Bundle data = intent.getExtras();
         data.putBoolean("tap", true);
 
-        FirebasePlugin.sendNotification(data, context);
+        //~ FirebasePlugin.sendNotification(data, context);
 
         launchIntent.putExtras(data);
         context.startActivity(launchIntent);
